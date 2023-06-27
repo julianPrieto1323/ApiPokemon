9ec509a5c29d48b24ed930b47986c9871c86ed14
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-27 16:39:48 +0200

    Añadida la funcionalidad de escribir y leer JSON de requests

--- JPV/JPV/src/main/java/com/SwapiPokemon/JPV/JSON.java
+++ JPV/JPV/src/main/java/com/SwapiPokemon/JPV/JSON.java
@@ -50,7 +50,57 @@ public class JSON {
             Gson gson = new Gson();
 
             //Creamos la clase que permite escribir en el fichero Json
-            Writer writer = new FileWriter("animales.json");
+            Writer writer = new FileWriter("pokemons.json");
+            try {
+                gson.toJson(lista, writer);
+                writer.close();
+            } catch (JsonIOException e) {
+                throw new RuntimeException(e);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ArrayList<Request> leerFicheroRequests(String ruta){
+        //En esta clase se lee el fichero JSON de menores de 60
+        ArrayList <Request> listaJSON = new ArrayList<Request>();
+        try {
+            //Creamos el objeto Gson
+            Gson gson = new Gson();
+
+            // Creamos el reader
+            Reader reader = null;
+            try {
+                reader = Files.newBufferedReader(Paths.get(ruta));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            //Usamos la libreria Gson para leer el fichero Json e introducirlo en un arraylist
+            listaJSON = new Gson().fromJson(reader, new TypeToken<ArrayList<Request>>() {}.getType());
+            System.out.println("FICHERO users.json LEIDO CORRECTAMENTE");
+
+            //Cerramos el reader
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return listaJSON;
+    }
+    public void escribirPeticion(ArrayList<Request> lista){
+        try {
+            // Creamos una nueva clase Json
+            Gson gson = new Gson();
+
+            //Creamos la clase que permite escribir en el fichero Json
+            Writer writer = new FileWriter("requests.json");
             try {
                 gson.toJson(lista, writer);
                 writer.close();
