f8a70791e28248e6400e455d47835e28ba9bfbca
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-06-27 16:48:50 +0200

    Funcionalidad del GET funcionanando, voy a guardar los pokemon registrados y cada request

--- JPV/JPV/src/main/java/com/SwapiPokemon/JPV/Controller.java
+++ JPV/JPV/src/main/java/com/SwapiPokemon/JPV/Controller.java
@@ -1,7 +1,14 @@
 package com.SwapiPokemon.JPV;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller{
+    DataHanding dataHanding = new DataHanding();
+    @GetMapping("/pokemon")
+    public Pokemon getPokemon(@RequestBody Request request){
+        return dataHanding.mostrarPokemon(request);
+    }
 }
