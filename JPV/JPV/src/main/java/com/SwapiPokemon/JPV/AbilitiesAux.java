3ee60e169c6fba322c64b449693773f8c5d40e3e
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-07-01 13:09:41 +0200

    Añadido a la clase Pokemon las habilidades y añadidas Habilidades AUX

--- JPV/JPV/src/main/java/com/SwapiPokemon/JPV/AbilitiesAux.java
+++ JPV/JPV/src/main/java/com/SwapiPokemon/JPV/AbilitiesAux.java
@@ -1,4 +1,26 @@
 package com.SwapiPokemon.JPV;
 
+import com.google.gson.Gson;
+
 public class AbilitiesAux {
+    private PokemonAbilities abiliy;
+
+    public AbilitiesAux() {
+    }
+
+    public AbilitiesAux(PokemonAbilities abiliy) {
+        this.abiliy = abiliy;
+    }
+
+    public PokemonAbilities getAbiliy() {
+        return abiliy;
+    }
+
+    public void setAbiliy(PokemonAbilities abiliy) {
+        this.abiliy = abiliy;
+    }
+    public String montarJson(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
 }
--- JPV/JPV/src/main/java/com/SwapiPokemon/JPV/PokemonAbilities.java
+++ JPV/JPV/src/main/java/com/SwapiPokemon/JPV/PokemonAbilities.java
@@ -1,5 +1,44 @@
 package com.SwapiPokemon.JPV;
 
+import com.google.gson.Gson;
+
 public class PokemonAbilities {
-    private
+    private String name;
+    private String url;
+
+    public PokemonAbilities() {
+    }
+
+    public PokemonAbilities(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "PokemonAbilities{" +
+                "name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+    public String JSON(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
 }
