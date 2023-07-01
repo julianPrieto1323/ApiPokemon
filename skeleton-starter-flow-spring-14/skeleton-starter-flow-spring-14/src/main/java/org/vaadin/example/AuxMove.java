a03622041025c68f75f0262852ec1904f116f2f7
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-07-01 13:57:06 +0200

    Copiadas las clases del Back

---
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/org/vaadin/example/AuxMove.java
@@ -0,0 +1,26 @@
+package org.vaadin.example;
+
+import com.google.gson.Gson;
+
+public class AuxMove {
+    private PokemonMove move;
+
+    public AuxMove() {
+    }
+
+    public AuxMove(PokemonMove move) {
+        this.move = move;
+    }
+
+    public PokemonMove getMove() {
+        return move;
+    }
+
+    public void setMove(PokemonMove move) {
+        this.move = move;
+    }
+    public String montarJSON(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+}
---
+++ skeleton-starter-flow-spring-14/skeleton-starter-flow-spring-14/src/main/java/org/vaadin/example/PokemonMove.java
@@ -0,0 +1,37 @@
+package org.vaadin.example;
+
+import com.google.gson.Gson;
+
+public class PokemonMove {
+    private String name;
+    private String url;
+
+    public PokemonMove() {
+    }
+
+    public PokemonMove(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String montarJSON(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+}
